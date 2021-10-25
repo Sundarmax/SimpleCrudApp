112b69d7419a153ca9537efb4128d69f953c6246
Sundarrajan R <sundarrajan.r@tavant.com>
2021-10-25 18:12:04 +0530

    heroku config added

--- SimpleCrudApp/settings.py
+++ SimpleCrudApp/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,11 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 django_heroku.settings(locals())
